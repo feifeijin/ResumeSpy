8cc0192639578f76da30ab29a39d82789d4ff7c0
Feifei <dsf_jyf@qq.com>
2025-02-17 15:34:54 +0900

    init

---
+++ Models/WeatherForecastModel.cs
@@ -0,0 +1,7 @@
+namespace ResumeSpy.Models
+{
+    public record WeatherForecastModel(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
